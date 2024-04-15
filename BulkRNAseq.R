2b67a29ad663f33d3cf2342a418ad19a1c720b30
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-15 11:36:02 +0330

    Update BulkRNAseq.R

--- BulkRNAseq.R
+++ BulkRNAseq.R
@@ -3,7 +3,7 @@ SamplesFile <- args[1]
 Samples_data <- read.table(SamplesFile, header = FALSE, stringsAsFactors = FALSE)
 labels <- factor(Samples_data$V2)
 
-
+labels <- factor(c("cancer", "healthy", "cancer"))
 library(DESeq2)
 library(ggplot2)
 library(pheatmap)
@@ -12,7 +12,7 @@ library(reshape2)
 library(plyr)
 options(stringAsFactors=F)
 files <- list.files("D:/NGS/Samples/P3/Aligned","*.count")
-#print(files)
+print(files)
 
 thecounts <- lapply(files, read.delim, header=F)
 thecounts <- do.call(cbind,thecounts)
@@ -20,7 +20,7 @@ rownames(thecounts) <- thecounts[,1]
 thecounts <- thecounts[,-seq(1,ncol(thecounts),2)]
 thecounts <- thecounts[-seq(nrow(thecounts)-4,nrow(thecounts)),] 
 colnames(thecounts) <- sub("_sorted.count","",files)
-#print(head(thecounts))
+print(head(thecounts))
 
 gr <- labels
 colData <- data.frame(group=gr,type="single-end")
@@ -31,7 +31,7 @@ cnt <- log2(1+counts(cds,normalized=T))
 dif <- data.frame(results(cds,c("group",levels(gr)[1],levels(gr)[2])))
 dif$padj <- p.adjust(dif$pvalue,method="BH")
 dif <- dif[order(dif$padj),]
-#print(head(dif))
+print(head(dif))
 
 vp <- ggplot(dif,aes(log2FoldChange,-log10(pvalue),color=log2FoldChange))+geom_point()+theme_bw()
 ggsave("volcano_plot.pdf", plot = vp, device = "pdf")
